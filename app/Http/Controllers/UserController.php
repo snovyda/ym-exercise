24855803665a753ecbd1e76cca781782ca5daddb
R. Mova <mova.r@yahoo.com>
2022-02-18 11:57:36 +0200

    Revert "Merge pull request #1 from snovyda/test_revert"
    This reverts commit a420af19e0da5ddbaf452fb1f61764e2bd7dc5ff, reversing
    changes made to 8254b1418e0ae727b212eae8fee971f4e3661c6d.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller
      */
     public function getCompanies()
     {
-        /* =============== TEST STRING ================= */
         $companies = User::find(auth()->id())
             ->companies()
             ->orderBy('title')
